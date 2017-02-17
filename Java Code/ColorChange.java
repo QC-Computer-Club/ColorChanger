a99012644d576afc01cc9356eafa8a8055a2372e
Alvin-QuincyCollege <alvin.xie@qc.quincycollege.edu>
2017-02-17 16:45:42 -0500

    Add files via upload
    Added the basic java files to run to modify images.

---
+++ Java Code/ColorChange.java
@@ -0,0 +1,46 @@
+package color;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+public class ColorChange {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        ColorChange ia = new ColorChange();  
+        JFrame frame = new JFrame("Tutorials");  
+        ia.loadAndDisplayImage(frame);  
+	}
+
+	public static BufferedImage loadImage(String ref) {  
+        BufferedImage bimg = null;  
+        try {  
+  
+            bimg = ImageIO.read(new File(ref));  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+        return bimg;  
+    }  
+    BufferedImage loadImg = null;
+
+    public void loadAndDisplayImage(JFrame frame) {  
+        // Load the img  
+    	try {
+    		loadImg = ImageIO.read(new File("E:\\Color\\bin\\color\\Untitled.png"));
+    	} catch (IOException e) {
+    	}
+        frame.setBounds(0, 0, loadImg.getWidth(), loadImg.getHeight());  
+        JImagePanel panel = new JImagePanel(loadImg, 0, 0);  
+        frame.add(panel);  
+        frame.setVisible(true);  
+    }  
+    
+    
+}
+
