2f8087ea2e15a8a36d6b0aa69f253c1df5d490eb
Alvin-QuincyCollege <alvin.xie@qc.quincycollege.edu>
2017-02-17 17:21:52 -0500

    Update ColorChange.java
    Modified the code and added comments for picking out colors and changing colors of an image.

--- Java Code/ColorChange.java
+++ Java Code/ColorChange.java
@@ -1,13 +1,15 @@
-package color;
+
 
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
+
+
 import javax.swing.JFrame;
+import color.*;
 public class ColorChange {
 	
 	public static void main(String[] args) {
@@ -15,6 +17,7 @@ public class ColorChange {
         ColorChange ia = new ColorChange();  
         JFrame frame = new JFrame("Tutorials");  
         ia.loadAndDisplayImage(frame);  
+         
 	}
 
 	public static BufferedImage loadImage(String ref) {  
@@ -27,12 +30,30 @@ public class ColorChange {
         }  
         return bimg;  
     }  
+    
+    public static BufferedImage changeImage(BufferedImage original) {  
+        int width = original.getWidth();
+        int height = original.getHeight();
+        
+        //Commands for creating a color you want to change to.
+        //Color colorName=new Color(int r,int g,int blue);
+        //Color colorName=new Color(int red,int green,int blue,int alpha);
+        
+        //Changing the color of a pixel at that exact location.
+        //image.setrgb(x,y,colorName)
+        File outputfile = new File("saved.png");
+        ImageIO.write(original, "png", outputfile);
+    }  
+    
+    
     BufferedImage loadImg = null;
 
     public void loadAndDisplayImage(JFrame frame) {  
         // Load the img  
     	try {
-    		loadImg = ImageIO.read(new File("E:\\Color\\bin\\color\\Untitled.png"));
+      
+      //Change the read in location.
+    		loadImg = ImageIO.read(new File("C:\\Users\\Instructor\\Desktop\\Color\\download.png"));
     	} catch (IOException e) {
     	}
         frame.setBounds(0, 0, loadImg.getWidth(), loadImg.getHeight());  
